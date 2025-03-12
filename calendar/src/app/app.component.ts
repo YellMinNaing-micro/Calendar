c7215528eaa99a366e3bc91242cbbfd2bb2efa2b
Yell Min Naing <117013435+YellMinNaing-micro@users.noreply.github.com>
2025-03-12 11:13:04 +0630

    Update app.component.ts

--- calendar/src/app/app.component.ts
+++ calendar/src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FullCalendarModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'calendar';
+  calendarOptions: CalendarOptions | undefined;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.calendarOptions = {
+        initialView: 'dayGridMonth', // Ensure this view is available
+        plugins: [dayGridPlugin, interactionPlugin], // Load required plugins
+        editable: true,
+        selectable: true,
+        events: [
+          { title: 'Event 1', date: '2025-03-07' },
+          { title: 'Event 2', date: '2025-03-12' }
+        ]
+      };
+    }
+  }
 }
