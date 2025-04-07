912ab06b3faf3295258c99911574a1ba8cd9d09a
Yell Min Naing <117013435+YellMinNaing-micro@users.noreply.github.com>
2025-04-07 17:16:20 +0630

    Create week.component.ts

---
+++ calendar/src/app/week/week.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-week',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './week.component.html',
+  styleUrl: './week.component.scss'
+})
+export class WeekComponent {
+
+}
